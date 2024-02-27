3999b664562d37d9db5bf6ec735d72e402c48ec3
farhan <farhan.dzyntech@gmail.com>
2024-02-27 22:30:11 +0500

    fix resume pdf issues

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -16,12 +16,14 @@ const ssPath = 'E:/Jobbook_Backend/uploads/pdfs/page-render.png';
   // Launch puppeteer browser
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
+  await page.setViewport({width: 2490, height: 3508});
   // await page.setContent(htmlContent);
   await page.setContent(htmlContent, {
     waitUntil: 'networkidle2'
   });
   await page.screenshot({path: ssPath});
-  await page.setViewport({width: 2480, height: 3508});
+  // await page.setViewport({width: 2480, height: 3508});
 
   // Generate PDF from HTML
   const pdfPath = `E:/Jobbook_Backend/uploads/pdfs/${data.temp}.pdf`;
@@ -29,11 +31,15 @@ const ssPath = 'E:/Jobbook_Backend/uploads/pdfs/page-render.png';
 //   const pdfPath = path.join(__dirname, '../public/pdfs', 'output.pdf');
   await page.pdf({ 
     path: pdfPath, 
-    format: 'A4', 
+    // format: 'A4',
     printBackground: true, 
     // margin: {top: 0, left: 0, right: 0, bottom: 0 }, 
     displayHeaderFooter: false,
-    margin: { top: '0px', right: '0px', bottom: '0px', left: '0px' },
+    // margin: { top: '0px', right: '0px', bottom: '0px', left: '0px' },
+    pageRanges: '1',
+    width: 2480,
+    height: 3508,
+    preferCSSPageSize: false
   });
   await browser.close();
   return pdfPath;
